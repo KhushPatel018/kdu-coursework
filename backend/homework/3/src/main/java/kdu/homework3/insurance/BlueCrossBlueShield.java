678e4868a4e2c083d1c6651b0606d86f808bc692
KhushPatel018 <patelkhush.girishbhai.kickdrumtech.com>
2024-01-10 23:01:52 +0530

    tried to remove cognitive complexity

--- backend/homework/3/src/main/java/kdu/homework3/insurance/BlueCrossBlueShield.java
+++ backend/homework/3/src/main/java/kdu/homework3/insurance/BlueCrossBlueShield.java
@@ -9,29 +9,29 @@ public class BlueCrossBlueShield implements InsuranceBrand {
 
     @Override
     public double computeMonthlyPremium(HealthInsurancePlan insurancePlan, int age, boolean smoking) {
-            double premium = 0;
-        try{
+        double premium = 0;
 
-            if(insurancePlan instanceof PlatinumPlan){
-                premium+= age > 55 ? 200 : 0;
-                premium+= smoking ? 100 : 0;
-            }else if(insurancePlan instanceof GoldPlan){
-                premium+= age > 55 ? 150 : 0;
-                premium+= smoking ? 90 : 0;
-            } else if (insurancePlan instanceof SilverPlan) {
-                premium+= age > 55 ? 100 : 0;
-                premium+= smoking ? 80 : 0;
-            } else if (insurancePlan instanceof BronzePlan) {
-                premium+= age > 55 ? 50 : 0;
-                premium+= smoking ? 70 : 0;
-            }else {
-                throw new NullPointerException("not taken any insurance plan");
-            }
-        }catch (NullPointerException e){
-            ls.logWarn(e.getMessage());
+        if (insurancePlan instanceof PlatinumPlan) {
+            premium += age > 55 ? 200 : 0;
+            premium += smoking ? 100 : 0;
+        } else if (insurancePlan instanceof GoldPlan) {
+            premium += age > 55 ? 150 : 0;
+            premium += smoking ? 90 : 0;
+        } else if (insurancePlan instanceof SilverPlan) {
+            premium += age > 55 ? 100 : 0;
+            premium += smoking ? 80 : 0;
+        } else if (insurancePlan instanceof BronzePlan) {
+            premium += age > 55 ? 50 : 0;
+            premium += smoking ? 70 : 0;
+        } else {
+            logInsurancePlanError();
         }
+
         return premium;
     }
 
+    private void logInsurancePlanError() {
+        ls.logWarn("Insurance plan not recognized or provided");
+    }
 
 }
