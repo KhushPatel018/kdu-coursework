1096a0c619ec905a16933361fcca7f4f15c207fd
KhushPatel018 <patelkhush.girishbhai.kickdrumtech.com>
2024-02-08 15:55:06 +0530

    removed comments

--- frontend/homework/3/q2.js
+++ frontend/homework/3/q2.js
@@ -1,14 +1,3 @@
-// Create a function that takes a string as an argument and returns a coded version of the string.
-// And remove spaces at the beginning and end of the string.
-// The function should replace all 'a's with 4, 'e's with 3, 'i's with 1, 'o's with 0, and 's's with 5.
-
-// Test Cases :
-
-// INPUT:  "javascript is cool  " OUTPUT "j4v45cr1pt 15 c00l"
-
-// INPUT:  "programming is fun" OUTPUT "pr0gr4mm1ng 15 fun"
-
-// INPUT:  "  become a coder" OUTPUT "b3c0m3 4 c0d3r"
 
 let replaceMap = {
   a: "4",
