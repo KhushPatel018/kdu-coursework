d1c90dac9d36fb2914228b14c0fdb3cc6fd3f3d1
KhushPatel018 <155981014+KhushPatel018@users.noreply.github.com>
2024-02-12 19:14:11 +0530

    Async Await

--- frontend/homework/5/index.js
+++ frontend/homework/5/index.js
@@ -3,27 +3,31 @@ const os = require("os");
 const express = require("express");
 const app = express();
 
+// Start the server
 app.listen(8081, () => {
   console.log("app is running in port 8080");
 });
 
-//a. Os :
-const createJson = () => {
+// a. Os:
+const createJson = async () => {
   const jsonObj = {
     hostName: os.hostname(),
     operatingSystem: os.platform(),
-    Architecture: os.arch(),
-    OSRelease: os.release(),
+    architecture: os.arch(),
+    osRelease: os.release(),
     uptime: os.uptime(),
     numberOfCPUCores: os.cpus().length,
     totalMemory: os.totalmem(),
     freeMemory: os.freemem(),
-    CurrentWorkingDirectory: process.cwd(),
+    currentWorkingDirectory: process.cwd(),
   };
-  console.log("A : OS RESULT OBJECT : \n", JSON.stringify(jsonObj, null, 2));
-  fs.writeFile("./test.json", JSON.stringify(jsonObj, null, 2))
-    .then(() => console.log("written to file"))
-    .catch((err) => console.log(err));
+  console.log("A: OS RESULT OBJECT: \n", JSON.stringify(jsonObj, null, 2));
+  try {
+    await fs.writeFile("./test.json", JSON.stringify(jsonObj, null, 2));
+    console.log("written to file");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const parseData = (data) => {
@@ -34,35 +38,31 @@ const parseData = (data) => {
     ${jsonString}.`;
 };
 
-// function call
 createJson();
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   console.log("get request called");
-  fs.readFile("./test.json", "utf-8")
-    .then((data) => res.status(200).send(parseData(data)))
-    .catch((err) => {
-      res.status(500).send("failed to recieve file with " + err);
-    });
+  try {
+    const data = await fs.readFile("./test.json", "utf-8");
+    res.status(200).send(parseData(data));
+  } catch (err) {
+    res.status(500).send("failed to receive file with " + err);
+  }
 });
 
-// b. Path :
-
+// b. Path:
 const path = require("path");
 
-function extractFileInfo(filePath) {
+const extractFileInfo = (filePath) => {
   return {
     extension: path.extname(filePath),
     baseName: path.basename(filePath),
     directory: path.dirname(filePath),
   };
-}
+};
 
 const filePath = "/home/hp/code/FrontEnd/NodeJs/handsOn/test.json";
-console.log(
-  "B. PATH RESULT OF extractFileInfo : \n",
-  JSON.stringify(extractFileInfo(filePath), null, 2)
-);
+console.log("B. PATH RESULT OF extractFileInfo: \n", JSON.stringify(extractFileInfo(filePath), null, 2));
 
 const filePaths = [
   "./dir1/dir2/file1.txt",
@@ -73,10 +73,11 @@ const filePaths = [
 ];
 
 const processFilePaths = (filePaths) => {
-  return filePaths.map(extractFileInfo);
+  const result = [];
+  filePaths.forEach((filePath) => {
+    result.push(extractFileInfo(filePath));
+  });
+  return result;
 };
 
-console.log(
-  "B. PATH RESULT OF processFilePaths : \n",
-  JSON.stringify(processFilePaths(filePaths), null, 2)
-);
+console.log("B. PATH RESULT OF processFilePaths: \n", JSON.stringify(processFilePaths(filePaths), null, 2));
